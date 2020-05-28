a721e790dbecf8d1dfd9ab35247b2d6e584376ee
Suganya108 <60962487+Suganya108@users.noreply.github.com>
2020-05-28 17:31:09 +0530

    Add files via upload

---
+++ 28-05-2020/news.js
@@ -0,0 +1,398 @@
+var body = document.getElementsByTagName('body')[0];
+var c = document.createElement('center');
+var d = document.createElement('div');
+d.style.width = '90%';
+d.style.backgroundColor = 'whitesmoke';
+c.appendChild(d);
+body.appendChild(c);
+var d1 = document.createElement('div');
+d1.className = 'd1';
+var p = document.createElement('p');
+p.style.marginTop = '0px';
+p.innerHTML = 'The Rising Sun';
+d1.appendChild(p);
+d.appendChild(d1)
+c.appendChild(d);
+body.appendChild(c);
+
+var d2 = document.createElement('div');
+d2.className = 'd2';
+var d1 = document.createElement('div');
+d1.className = 'subdiv1';
+var i = document.createElement('img');
+i.style.width = '80px';
+i.style.height = '80px';
+i.style.marginTop = '30px';
+i.src = 'car.jpg';
+d1.appendChild(i);
+d2.appendChild(d1);
+var p = document.createElement('p');
+p.style.fontSize = '12px';
+p.innerHTML = 'On page 16';
+d1.appendChild(p);
+d2.appendChild(d1);
+
+var d1 = document.createElement('div');
+d1.className = 'subdiv2';
+var p = document.createElement('p');
+p.style.fontSize = '12px';
+p.style.marginTop = '55px';
+p.innerHTML = 'The use of electric cars, trucks, and buses is on the rise.';
+d1.appendChild(p);
+d2.appendChild(d1);
+
+d.appendChild(d2)
+c.appendChild(d);
+body.appendChild(c);
+
+//>>>
+
+var d3 = document.createElement('div');
+d3.className = 'd3';
+var d1 = document.createElement('div');
+d1.className = 'subdiv1';
+d1.style.marginTop = '-150px';
+var i = document.createElement('img');
+i.style.width = '80px';
+i.style.height = '80px';
+i.style.marginTop = '30px';
+i.src = 'car.jpg';
+d1.appendChild(i);
+d2.appendChild(d1);
+var p = document.createElement('p');
+p.style.fontSize = '12px';
+p.innerHTML = 'On page 16';
+d1.appendChild(p);
+d3.appendChild(d1);
+
+var d1 = document.createElement('div');
+d1.className = 'subdiv2';
+var p = document.createElement('p');
+p.style.fontSize = '12px';
+p.style.marginTop = '-100px';
+p.innerHTML = 'The use of electric cars, trucks, and buses is on the rise.';
+d1.appendChild(p);
+d3.appendChild(d1);
+
+d.appendChild(d3)
+c.appendChild(d);
+body.appendChild(c);
+
+
+var d4 = document.createElement('div');
+d4.className = 'd4';
+var p = document.createElement('p');
+p.style.fontSize = '12px';
+
+p.innerHTML += '<b style="margin-left: 5px;float: inline-start;">Wednesday October 24, 2018  </b>&nbsp&nbsp&nbsp&nbspFlorida / United States&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<b style="margin-left: 280px;"><u>www.therisingsun.org</u><span style="margin-left: 310px;">Volume 34 / Price $1 / Page 01 of 23</span></b>';
+d4.appendChild(p);
+
+
+//d.appendChild(d4)
+//c.appendChild(d);
+body.appendChild(d4);
+
+var c = document.createElement('center');
+var d = document.createElement('div');
+d.style.width = '100%';
+d.style.height = '1000px';
+d.style.backgroundColor = 'whitesmoke';
+c.appendChild(d);
+body.appendChild(c);
+
+var d1 = document.createElement('div');
+d1.className = 'mdiv1';
+var dd1 = document.createElement('div');
+dd1.style.width = '100%';
+dd1.style.height = '500px';
+var p1 = document.createElement('p');
+p1.className = 'p1';
+p1.innerHTML = 'ELECTRIC CARS: THE FUTURE<br>OF TRANSPORTATION';
+dd1.appendChild(p1);
+d1.appendChild(dd1);
+var i = document.createElement('img');
+i.style.width = '98%';
+i.style.marginLeft = '-10px';
+i.style.height = '200px';
+i.style.marginTop = '-10px';
+i.src = 'car.jpg';
+dd1.appendChild(i);
+d1.appendChild(dd1);
+var p = document.createElement('p');
+p.className = 'content';
+p.innerHTML = 'SEP 19, 2019  BETHANY KWOKA | @BKWOKA<br><br>The use of electric cars, trucks, and buses is on the rise. And that’s good, since nearly 30% of climate-damaging emissions nationwide come from transportation. (That percentage is even higher in New England states like Massachusetts.) Switching to electric cars and powering them with clean energy like solar and wind will slash our emissions and help us avoid the worst effects of climate change.Electric cars don’t spew the tailpipe pollution that’s linked to asthma, heart attacks, poor health, and premature death. Electric cars, trucks, and buses also don’t release unhealthy tailpipe pollution while idling in your neighborhood, stuck in traffic.<br><br> You can help these cars truly become the future of transportation in a few ways. To start, pledge to go electric when buying your next car! As well, demand electric options at your local dealership–so you can test drive one as well as purchase one the next time you’re in the market for a vehicle. Additionally, share the graphic below on social media so that everyone in your network can do the same.<br><span class="page"><i>contents at page 66</i></span>';
+dd1.appendChild(p);
+d1.appendChild(dd1);
+var dd2 = document.createElement('div');
+dd2.style.width = '100%';
+dd2.style.height = '2px';
+dd2.style.backgroundColor = 'black';
+dd2.style.marginTop = '50px';
+d1.appendChild(dd2);
+var p = document.createElement('p');
+p.className = 'p2';
+p.innerHTML = '<u>NEWS AROUND THE WORLD</u>';
+d1.appendChild(p);
+
+var dd3 = document.createElement('div');
+dd3.style.width = '100%';
+dd3.style.marginTop = '-50px';
+dd3.style.height = '500px';
+var dr1 = document.createElement('div');
+dr1.className = 'top';
+var drc1 = document.createElement('div');
+drc1.className = 'topleft';
+var p = document.createElement('p');
+p.className = 'p3';
+p.innerHTML = 'WORLD\'S FIRST UNDERWATER HOTEL VILLA';
+drc1.appendChild(p);
+dr1.appendChild(drc1);
+dd3.appendChild(dr1);
+var rcc = document.createElement('div');
+rcc.className = 'blef';
+rcc.marginTop = '-10px';
+var i = document.createElement('img');
+i.className = 'i1';
+i.src = 'rc1.jpg';
+rcc.appendChild(i);
+drc1.appendChild(rcc);
+dr1.appendChild(drc1);
+dd3.appendChild(dr1);
+var con = document.createElement('div');
+var p = document.createElement('p');
+p.className = 'content';
+p.style.marginTop = '25px';
+p.innerHTML = 'The Conrad Maldives Rangali Island has opened the world\'s first-ever underwater hotel residence, fulfilling Little Mermaid dreams everywhere. Submerged more than 16 feet below the Indian Ocean, the two-story villa goes by the moniker Muraka, which translates to “coral” in the Maldivian language of Dhivehi. ';
+con.appendChild(p);
+//rcc.appendChild(con);
+drc1.appendChild(con);
+dr1.appendChild(drc1);
+dd3.appendChild(dr1);
+
+
+var drc1 = document.createElement('div');
+drc1.className = 'topright';
+var p = document.createElement('p');
+p.className = 'p3';
+p.innerHTML = 'THE TALLEST STATUE IN THE WORLD';
+drc1.appendChild(p);
+dr1.appendChild(drc1);
+dd3.appendChild(dr1);
+var rcc = document.createElement('div');
+rcc.className = 'blef';
+rcc.marginTop = '-10px';
+var i = document.createElement('img');
+i.className = 'i1';
+i.src = 'mob.jpg';
+rcc.appendChild(i);
+drc1.appendChild(rcc);
+dr1.appendChild(drc1);
+dd3.appendChild(dr1);
+var con = document.createElement('div');
+var p = document.createElement('p');
+p.className = 'content';
+p.style.marginTop = '25px';
+p.innerHTML = 'The Conrad Maldives Rangali Island has opened the world\'s first-ever underwater hotel residence, fulfilling Little Mermaid dreams everywhere. Submerged more than 16 feet below the Indian Ocean, the two-story villa goes by the moniker Muraka, which translates to “coral” in the Maldivian language of Dhivehi. ';
+con.appendChild(p);
+//rcc.appendChild(con);
+drc1.appendChild(con);
+dr1.appendChild(drc1);
+dd3.appendChild(dr1);
+
+d1.appendChild(dd3);
+
+
+var drc1 = document.createElement('div');
+drc1.className = 'bottomleft';
+var p = document.createElement('p');
+p.className = 'p3';
+p.innerHTML = 'ELECTRIC CARS: THE FUTURE OF TRANSPORTATION';
+drc1.appendChild(p);
+dr1.appendChild(drc1);
+dd3.appendChild(dr1);
+var rcc = document.createElement('div');
+rcc.className = 'blef';
+rcc.marginTop = '-10px';
+var i = document.createElement('img');
+i.className = 'i1';
+i.src = 'rc2.jpg';
+rcc.appendChild(i);
+drc1.appendChild(rcc);
+dr1.appendChild(drc1);
+dd3.appendChild(dr1);
+var con = document.createElement('div');
+var p = document.createElement('p');
+p.className = 'content';
+p.style.marginTop = '25px';
+p.innerHTML = 'The Conrad Maldives Rangali Island has opened the world\'s first-ever underwater hotel residence, fulfilling Little Mermaid dreams everywhere. Submerged more than 16 feet below the Indian Ocean, the two-story villa goes by the moniker Muraka, which translates to “coral” in the Maldivian language of Dhivehi. ';
+con.appendChild(p);
+//rcc.appendChild(con);
+drc1.appendChild(con);
+dr1.appendChild(drc1);
+dd3.appendChild(dr1);
+d1.appendChild(dd3);
+
+var drc1 = document.createElement('div');
+drc1.className = 'bottomright';
+var p = document.createElement('p');
+p.className = 'p3';
+p.innerHTML = 'SUICIDE BOMB ATTCK AT <br>TUMS';
+drc1.appendChild(p);
+dr1.appendChild(drc1);
+dd3.appendChild(dr1);
+var rcc = document.createElement('div');
+rcc.className = 'blef';
+rcc.marginTop = '-10px';
+var i = document.createElement('img');
+i.className = 'i1';
+i.src = 'rc3.jpg';
+rcc.appendChild(i);
+drc1.appendChild(rcc);
+dr1.appendChild(drc1);
+dd3.appendChild(dr1);
+var con = document.createElement('div');
+var p = document.createElement('p');
+p.className = 'content';
+p.style.marginTop = '25px';
+p.innerHTML = 'The Conrad Maldives Rangali Island has opened the world\'s first-ever underwater hotel residence, fulfilling Little Mermaid dreams everywhere. Submerged more than 16 feet below the Indian Ocean, the two-story villa goes by the moniker Muraka, which translates to “coral” in the Maldivian language of Dhivehi. ';
+con.appendChild(p);
+//rcc.appendChild(con);
+drc1.appendChild(con);
+dr1.appendChild(drc1);
+dd3.appendChild(dr1);
+
+d1.appendChild(dd3);
+
+var dd2 = document.createElement('div');
+dd2.style.width = '100%';
+dd2.style.height = '2px';
+dd2.style.backgroundColor = 'black';
+dd2.style.marginTop = '30px';
+d1.appendChild(dd2);
+
+var dd4 = document.createElement('div');
+dd4.style.width = '100%';
+dd4.style.height = '200px';
+var p = document.createElement('p');
+p.className = 'p4';
+p.innerHTML = 'TEN BOOKS TO CHECK OUT THIS AUTUMN';
+dd4.appendChild(p);
+d1.appendChild(dd4);
+
+var rcc = document.createElement('div');
+rcc.className = 'bleft';
+rcc.marginTop = '-10px';
+var i = document.createElement('img');
+i.className = 'i3';
+i.src = 'rc3.jpg';
+rcc.appendChild(i);
+dd4.appendChild(rcc);
+d1.appendChild(dd4);
+var con = document.createElement('div');
+con.className = 'bright';
+var p = document.createElement('p');
+p.className = 'content';
+p.style.marginTop = '25px';
+p.innerHTML = 'The Conrad Maldives Rangali Island has opened the world\'s first-ever underwater hotel residence, fulfilling Little Mermaid dreams everywhere. Submerged more than 16 feet below the Indian Ocean, the two-story villa goes by the moniker Muraka, which translates to “coral” in the Maldivian language of Dhivehi. The Conrad Maldives Rangali Island has opened the world\'s first-ever underwater hotel residence, fulfilling Little Mermaid dreams everywhere. Submerged more than 16 feet below the Indian Ocean, the two-story villa goes by the moniker Muraka, which translates to “coral” in the Maldivian language of Dhivehi.The Conrad Maldives Rangali Island has opened the world\'s first-ever underwater hotel residence, fulfilling Little Mermaid dreams everywhere. Submerged more than 16 feet below the Indian Ocean, the two-story villa goes by the moniker Muraka, which translates to “coral” in the Maldivian language of Dhivehi.The Conrad Maldives Rangali Island has opened the world\'s first-ever underwater hotel residence, fulfilling Little Mermaid dreams everywhere. Submerged more than 16 feet below the Indian Ocean, the two-story villa goes by the moniker Muraka, which translates to “coral” in the Maldivian language of Dhivehi. ';
+con.appendChild(p);
+//rcc.appendChild(con);
+dd4.appendChild(con);
+d1.appendChild(dd4);
+
+d.appendChild(d1);
+c.appendChild(d);
+body.appendChild(c);
+
+var d1 = document.createElement('div');
+d1.className = 'mdiv2';
+
+var dd1 = document.createElement('div');
+dd1.style.width = '100%';
+dd1.style.height = '510px';
+var p = document.createElement('p');
+p.className = 'p6';
+p.innerHTML = 'POLITICS';
+dd1.appendChild(p);
+d1.appendChild(dd1);
+var i = document.createElement('img');
+i.className = 'i4';
+i.src = 'flg.jpg';
+dd1.appendChild(i);
+d1.appendChild(dd1);
+var p = document.createElement('p');
+p.className = 'p5';
+p.innerHTML = 'A CHINESE COMPANY TO BUILD A FAKE MOON TO REPLACE THE CITY';
+dd1.appendChild(p);
+d1.appendChild(dd1);
+
+var p = document.createElement('p');
+p.className = 'content';
+p.innerHTML = 'By Nick Mafi October 22, 2018';
+p.style.marginTop = '-12px';
+dd1.appendChild(p);
+d1.appendChild(dd1);
+var p = document.createElement('p');
+p.className = 'content';
+p.innerHTML = 'Come 2020, for children living in Chengdu, China, the book Goodnight Moon may have a completely different meaning. Indeed, according to an article in the People’s Daily (a widely distributed online Chinese newspaper), a local company plans to build an artificial moon that, if completed,would be eight times brighter than the actual moon. <br><br>To address concerns about the artificial moon’s affect on wildlife, a spokesman for the company said in a quote to the People’s Daily that “the light of the satellite is similar to a dusk-like glow, so it should not affect animals’ routines.” Yet, something about creating a moon-like satellite that\'s eight times brighter than the actual moon is leaving some wondering what a “dusk-like” glow will look like.<br><br><i>contents at page 67</i>';
+p.style.marginTop = '12px';
+dd1.appendChild(p);
+d1.appendChild(dd1);
+d.appendChild(d1);
+
+var dd2 = document.createElement('div');
+dd2.className = 'box';
+d1.appendChild(dd2);
+var db = document.createElement('div');
+db.className = 'db';
+dd2.appendChild(db);
+d1.appendChild(dd2);
+d.appendChild(d1);
+
+var p = document.createElement('p');
+p.className = 'h';
+p.innerHTML = '# MEETOO MOVEMENT IN INDIA GETS BOOM';
+db.appendChild(p);
+dd2.appendChild(db);
+d1.appendChild(dd2);
+d.appendChild(d1);
+var p = document.createElement('p');
+p.className = 'content';
+p.innerHTML = 'In India, the Me Too movement is seen as either an independent outgrowth influenced by the international campaign against sexual harassment of women in the workplace, or an offshoot of the American "Me Too" social movement. Me Too began gaining prominence in India with the increasing popularity of the international movement, <br><br><i>contents at page 68</i>';
+db.appendChild(p);
+dd2.appendChild(db);
+d1.appendChild(dd2);
+d.appendChild(d1);
+
+var p = document.createElement('p');
+p.className = 'h';
+p.innerHTML = 'PLANE CRASHES IN INDONESIA';
+db.appendChild(p);
+dd2.appendChild(db);
+d1.appendChild(dd2);
+d.appendChild(d1);
+var p = document.createElement('p');
+p.className = 'content';
+p.innerHTML = 'In India, the Me Too movement is seen as either an independent outgrowth influenced by the international campaign against sexual harassment of women in the workplace, or an offshoot of the American "Me Too" social movement. Me Too began gaining prominence in India with the increasing popularity of the international movement, <br><br><i>contents at page 68</i>';
+db.appendChild(p);
+dd2.appendChild(db);
+d1.appendChild(dd2);
+d.appendChild(d1);
+
+var p = document.createElement('p');
+p.className = 'h';
+p.innerHTML = 'CAN HUMANS SURVIVE IN MARS ?';
+db.appendChild(p);
+dd2.appendChild(db);
+d1.appendChild(dd2);
+d.appendChild(d1);
+var p = document.createElement('p');
+p.className = 'content';
+p.innerHTML = 'Organizations have proposed plans for a human mission to Mars, the first step towards any colonization effort, but no person has set foot on the planet. However, landers and rovers have successfully explored the planetary surface and delivered information about conditions on the ground. <br><br><i>contents at page 70</i>';
+db.appendChild(p);
+dd2.appendChild(db);
+d1.appendChild(dd2);
+d.appendChild(d1);
+
+
+c.appendChild(d);
+body.appendChild(c);
